75c5bca911e3dc9649ae1e9651498f3a251cd6e1
Miran Legin <miran@ador.hr>
2025-03-25 13:28:59 +0100

    [Update]: cart item layout and positioning of elements inside

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,32 +9,38 @@ const CartItem = ({ item, itemQuantity, onRemove }) => {
 
   return (
     <li className='cart-item'>
-      <div style={{ flex: 1 }}>
-        <h3 className='text-preset-4 fw-semibold text-rose-900'>{item.name}</h3>
-        <p>
-          <span className='text-preset-4 fw-semibold text-red'>
-            {itemQuantity}x
-          </span>{' '}
-          <span className='text-preset-4 text-rose-500'>
-            @${item.price.toFixed(2)}
-          </span>
-          <span className='text-preset-4 fw-semibold text-rose-500'>
-            ${calculatedPrice}
-          </span>
-        </p>
+      <div className='cart-item-layout'>
+        <div className='cart-item-column'>
+          <h3 className='text-preset-4 fw-semibold text-rose-900'>
+            {item.name}
+          </h3>
+          <div className='cart-item-pricing'>
+            <span className='text-preset-4 fw-semibold text-red'>
+              {itemQuantity}x
+            </span>
+            <span className='text-preset-4 text-rose-500'>
+              @${item.price.toFixed(2)}
+            </span>
+            <span className='text-preset-4 fw-semibold text-rose-500'>
+              ${calculatedPrice}
+            </span>
+          </div>
+        </div>
+        <div>
+          <button
+            className='button-square'
+            data-type='remove'
+            onClick={() => {
+              onRemove(item.id);
+            }}
+          >
+            <span className='sr-only'>Remove {item.name} </span>
+            <svg className='icon icon-remove'>
+              <use xlinkHref='/assets/icons/symbol-defs.svg#icon-remove'></use>
+            </svg>
+          </button>
+        </div>
       </div>
-      <button
-        className='button-square'
-        data-type='remove'
-        onClick={() => {
-          onRemove(item.id);
-        }}
-      >
-        <span className='sr-only'>Remove {item.name} </span>
-        <svg className='icon icon-remove'>
-          <use xlinkHref='/assets/icons/symbol-defs.svg#icon-remove'></use>
-        </svg>
-      </button>
     </li>
   );
 };
